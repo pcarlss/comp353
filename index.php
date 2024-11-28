dffcdf505fc506b5a94640df8e8fa80a8f2f9390
pcarlss <philipcarlssoncoulombe@gmail.com>
2024-11-27 19:54:25 -0500

    hugo merge

--- index.php
+++ index.php
@@ -167,7 +167,7 @@ session_start();
     <!-- Top Bar -->
     <div class="top-bar">
         <h1>Home</h1>
-        <a href="community_tab.php">
+        <a href="groups/community_tab.php" style="margin-left: 70%">
             <button>
                 <h3>Join a Community</h3>
             </button>

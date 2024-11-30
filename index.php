f10f60371ef4b8619ed02e880fd07718f9adf7e7
CeeLiZiB <116758072+CeeLiZiB@users.noreply.github.com>
2024-11-29 22:52:31 -0500

    Update index.php

--- index.php
+++ index.php
@@ -167,7 +167,7 @@ session_start();
     <!-- Top Bar -->
     <div class="top-bar">
         <h1>Home</h1>
-        <a href="groups/community_tab.php" style="margin-left: 60%">
+        <a href="groups/community_tab.php" style="margin-left: 50%">
             <button>
                 <h3>Join a Community</h3>
             </button>

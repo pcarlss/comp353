ec0f3d99eaa0da268b335a6505f8c1c78d8e2371
Aboubacry <123341839+Aboubacry97@users.noreply.github.com>
2024-12-03 05:28:19 -0500

    Update Database_updated.sql

--- database files/Database_updated.sql
+++ database files/Database_updated.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Nov 08, 2024 at 01:15 AM
+-- Generation Time: Dec 03, 2024 at 11:27 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `project`
+-- Database: `mnmnnmmn`
 --
 
 -- --------------------------------------------------------
@@ -26,7 +26,6 @@ SET time_zone = "+00:00";
 --
 -- Table structure for table `AccessControl`
 --
-DROP TABLE IF EXISTS `AccessControl`;
 
 CREATE TABLE `AccessControl` (
   `AccessID` int(50) NOT NULL,
@@ -51,22 +50,6 @@ CREATE TABLE `Blocked` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Privacy`
---
-DROP TABLE IF EXISTS `Privacy`;
-CREATE TABLE `Privacy` (
-  `PrivacyID` int(50) NOT NULL,
-  `Fname` tinyint(1) NOT NULL DEFAULT 0,
-  `Lname` tinyint(1) NOT NULL DEFAULT 0,
-  `BirthDate` tinyint(1) NOT NULL DEFAULT 0,
-  `pCity` tinyint(1) NOT NULL DEFAULT 0,
-  `pCountry` tinyint(1) NOT NULL DEFAULT 0,
-  `Work` tinyint(1) NOT NULL DEFAULT 0,
-  `pStatus` tinyint(1) NOT NULL DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
--- --------------------------------------------------------
-
---
 -- Table structure for table `Comment`
 --
 
@@ -85,13 +68,12 @@ CREATE TABLE `Comment` (
 --
 
 CREATE TABLE `Event` (
-  `EventID` int(50) NOT NULL AUTO_INCREMENT,
+  `EventID` int(50) NOT NULL,
   `EventName` varchar(50) NOT NULL,
   `EventDesc` varchar(50) NOT NULL,
   `EventCreatorID` int(50) NOT NULL,
-  `EventGroupID` int(50) NULL,
-  `EventPostedAt` datetime NOT NULL DEFAULT NOW(),
-  PRIMARY KEY (`EventID`)
+  `EventGroupID` int(50) DEFAULT NULL,
+  `EventPostedAt` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -101,11 +83,10 @@ CREATE TABLE `Event` (
 --
 
 CREATE TABLE `EventOptions` (
-  `OptionID` int(50) NOT NULL AUTO_INCREMENT,
+  `OptionID` int(50) NOT NULL,
   `EventID` int(50) NOT NULL,
   `Date_Time_Location` varchar(50) NOT NULL,
-  `Votes` int(50) NOT NULL DEFAULT 0,
-  PRIMARY KEY (`OptionID`)
+  `Votes` int(50) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -141,11 +122,10 @@ CREATE TABLE `Friendship` (
 --
 
 CREATE TABLE `Gift` (
-  `GiftID` int(50) NOT NULL AUTO_INCREMENT,
+  `GiftID` int(50) NOT NULL,
   `GiftExchangeEventID` int(50) NOT NULL,
   `GiftName` text NOT NULL,
-  `GiftforID` int(50) NOT NULL,
-  PRIMARY KEY (`GiftID`)
+  `GiftforID` int(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -155,63 +135,17 @@ CREATE TABLE `Gift` (
 --
 
 CREATE TABLE `GiftExchange` (
-  `GiftExchangeID` int(50) NOT NULL AUTO_INCREMENT,
-  `MemberID` int(50) NOT NULL,               -- Member who created the gift exchange
-  `GiftGroupID` int(50) NULL,            -- Uncommented this line
+  `GiftExchangeID` int(50) NOT NULL,
+  `MemberID` int(50) NOT NULL,
+  `GiftGroupID` int(50) DEFAULT NULL,
   `GiftExchangeName` varchar(50) NOT NULL,
   `GiftExchangeDesc` varchar(50) NOT NULL,
   `GiftExchangeDate` varchar(50) NOT NULL,
-  `GiftExchangeCreatedAt` datetime NOT NULL,
-  PRIMARY KEY (`GiftExchangeID`)
+  `GiftExchangeCreatedAt` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
---
--- Table structure for table `Member`
---
-CREATE TABLE `Member` (
-  `MemberID` int(50) NOT NULL,
-  `Username` varchar(50) NOT NULL,
-  `Password` varchar(50) NOT NULL,
-  `FirstName` varchar(50) NOT NULL,
-  `LastName` varchar(50) NOT NULL,
-  `DateOfBirth` date DEFAULT NULL,
-  `City` varchar(50) DEFAULT NULL,
-  `Country` varchar(50) DEFAULT NULL,
-  `Email` varchar(50) NOT NULL,
-  `Profession` varchar(50) DEFAULT NULL,
-  `Privilege` enum('Administrator','Senior','Junior') NOT NULL,
-  `Status` enum('Active','Inactive','Suspended') NOT NULL,
-  `BusinessAccount` tinyint(1) DEFAULT NULL,
-  `UserCreatedAt` date NOT NULL,
-  `UserUpdatedAt` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
-
-
-INSERT INTO `Member` (`MemberID`, `Username`, `Password`, `FirstName`, `LastName`, `DateOfBirth`, `City`, `Country`, `Email`, `Profession`, `Privilege`, `Status`, `BusinessAccount`, `UserCreatedAt`, `UserUpdatedAt`) VALUES
-(1, 'testuser', 'testpassword', 'Test', 'User', NULL, NULL, NULL, 'testuser@example.com', NULL, 'Administrator', 'Active', 0, '2024-11-06', '2024-11-06');
-
---
--- Triggers `Member`
---
-DELIMITER $$
-CREATE TRIGGER `after_member_insert` AFTER INSERT ON `Member` FOR EACH ROW BEGIN
-    INSERT INTO Privacy (PrivacyID) VALUES (NEW.MemberID);
-END
-$$
-DELIMITER ;
-
-
-
---
--- Indexes for table `Member`
---
-ALTER TABLE `Member`
-  ADD PRIMARY KEY (`MemberID`),
-  ADD UNIQUE KEY `UniqueEmail` (`Email`),
-  ADD UNIQUE KEY `UniqueUsername` (`Username`) USING BTREE;
 --
 -- Table structure for table `GroupList`
 --
@@ -224,12 +158,6 @@ CREATE TABLE `GroupList` (
   `GroupUpdatedAt` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Indexes for table `GroupList`
---
-ALTER TABLE `GroupList`
-  ADD PRIMARY KEY (`GroupID`),
-  ADD KEY `OwnerID` (`OwnerID`);
 -- --------------------------------------------------------
 
 --
@@ -245,29 +173,58 @@ CREATE TABLE `GroupMember` (
 
 -- --------------------------------------------------------
 
-
 --
--- Table structure for table `JoinRequest` to request to join a group
+-- Table structure for table `JoinRequests`
 --
 
 CREATE TABLE `JoinRequests` (
-  RequestID INT(50) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  GroupID INT(50) NOT NULL,
-  MemberID INT(50) NOT NULL,
-  RequestDate DATE NOT NULL,
-  FOREIGN KEY (GroupID) REFERENCES GroupList(GroupID) ON DELETE CASCADE,
-  FOREIGN KEY (MemberID) REFERENCES Member(MemberID) ON DELETE CASCADE
-);
-
-ALTER TABLE `JoinRequests` DROP FOREIGN KEY `joinrequests_ibfk_1`;
-
-ALTER TABLE `GroupList`
-MODIFY `GroupID` int(50) NOT NULL AUTO_INCREMENT;
-
-ALTER TABLE `JoinRequests`
-ADD CONSTRAINT `joinrequests_ibfk_1` FOREIGN KEY (`GroupID`) REFERENCES `GroupList`(`GroupID`) ON DELETE CASCADE ON UPDATE CASCADE;
+  `RequestID` int(50) NOT NULL,
+  `GroupID` int(50) NOT NULL,
+  `MemberID` int(50) NOT NULL,
+  `RequestDate` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
+
+--
+-- Table structure for table `Member`
+--
+
+CREATE TABLE `Member` (
+  `MemberID` int(50) NOT NULL,
+  `Username` varchar(50) NOT NULL,
+  `Password` varchar(50) NOT NULL,
+  `FirstName` varchar(50) NOT NULL,
+  `LastName` varchar(50) NOT NULL,
+  `DateOfBirth` date DEFAULT NULL,
+  `City` varchar(50) DEFAULT NULL,
+  `Country` varchar(50) DEFAULT NULL,
+  `Email` varchar(50) NOT NULL,
+  `Profession` varchar(50) DEFAULT NULL,
+  `Privilege` enum('Administrator','Senior','Junior') NOT NULL,
+  `Status` enum('Active','Inactive','Suspended') NOT NULL,
+  `BusinessAccount` tinyint(1) DEFAULT NULL,
+  `UserCreatedAt` date NOT NULL,
+  `UserUpdatedAt` date NOT NULL,
+  `ProfilePic` varchar(255) DEFAULT 'uploads/images/default_pfp.png'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `Member`
+--
+
+INSERT INTO `Member` (`MemberID`, `Username`, `Password`, `FirstName`, `LastName`, `DateOfBirth`, `City`, `Country`, `Email`, `Profession`, `Privilege`, `Status`, `BusinessAccount`, `UserCreatedAt`, `UserUpdatedAt`, `ProfilePic`) VALUES
+(1, 'testuser', 'testpassword', 'Test', 'User', NULL, NULL, NULL, 'testuser@example.com', NULL, 'Administrator', 'Active', 0, '2024-11-06', '2024-11-06', 'uploads/images/default_pfp.png');
+
+--
+-- Triggers `Member`
+--
+DELIMITER $$
+CREATE TRIGGER `after_member_insert` AFTER INSERT ON `Member` FOR EACH ROW BEGIN
+    INSERT INTO Privacy (PrivacyID) VALUES (NEW.MemberID);
+END
+$$
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -290,14 +247,48 @@ CREATE TABLE `Message` (
 --
 
 CREATE TABLE `Post` (
-  `PostID` int(50) NOT NULL AUTO_INCREMENT,  -- Auto-incrementing primary key
-  `MemberID` int(50) NOT NULL,               -- Member who created the post
-  `PostText` text NOT NULL,                  -- Content of the post
-  `PostImages` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`PostImages`)),  -- Post images in JSON format
-  `PostedAt` date NOT NULL,                  -- Date when the post was made
-  `Visibility` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`Visibility`)), -- Visibility settings in JSON format
-  `PostType` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,  -- Type of the post (e.g., text, image, etc.)
-  PRIMARY KEY (`PostID`)                     -- Primary key for the table
+  `PostID` int(50) NOT NULL,
+  `MemberID` int(50) NOT NULL,
+  `PostText` text NOT NULL,
+  `PostImages` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`PostImages`)),
+  `PostedAt` date NOT NULL,
+  `Visibility` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`Visibility`)),
+  `PostType` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Privacy`
+--
+
+CREATE TABLE `Privacy` (
+  `PrivacyID` int(50) NOT NULL,
+  `Fname` tinyint(1) NOT NULL DEFAULT 0,
+  `Lname` tinyint(1) NOT NULL DEFAULT 0,
+  `BirthDate` tinyint(1) NOT NULL DEFAULT 0,
+  `pCity` tinyint(1) NOT NULL DEFAULT 0,
+  `pCountry` tinyint(1) NOT NULL DEFAULT 0,
+  `Work` tinyint(1) NOT NULL DEFAULT 0,
+  `pStatus` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `Privacy`
+--
+
+INSERT INTO `Privacy` (`PrivacyID`, `Fname`, `Lname`, `BirthDate`, `pCity`, `pCountry`, `Work`, `pStatus`) VALUES
+(1, 0, 0, 0, 0, 0, 0, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `PromotionRequests`
+--
+
+CREATE TABLE `PromotionRequests` (
+  `RequestID` int(50) NOT NULL,
+  `MemberID` int(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -334,12 +325,6 @@ ALTER TABLE `Blocked`
   ADD KEY `MemberID2` (`MemberID2`);
 
 --
--- Indexes for table `Privacy`
---
-ALTER TABLE `Privacy`
-  ADD PRIMARY KEY (`PrivacyID`);
-
---
 -- Indexes for table `Comment`
 --
 ALTER TABLE `Comment`
@@ -351,6 +336,7 @@ ALTER TABLE `Comment`
 -- Indexes for table `Event`
 --
 ALTER TABLE `Event`
+  ADD PRIMARY KEY (`EventID`),
   ADD KEY `EventCreatorID` (`EventCreatorID`),
   ADD KEY `EventGroupID` (`EventGroupID`);
 
@@ -358,6 +344,7 @@ ALTER TABLE `Event`
 -- Indexes for table `EventOptions`
 --
 ALTER TABLE `EventOptions`
+  ADD PRIMARY KEY (`OptionID`),
   ADD KEY `EventID` (`EventID`);
 
 --
@@ -380,6 +367,7 @@ ALTER TABLE `Friendship`
 -- Indexes for table `Gift`
 --
 ALTER TABLE `Gift`
+  ADD PRIMARY KEY (`GiftID`),
   ADD KEY `GiftExchangeEventID` (`GiftExchangeEventID`),
   ADD KEY `GiftforID` (`GiftforID`);
 
@@ -387,8 +375,15 @@ ALTER TABLE `Gift`
 -- Indexes for table `GiftExchange`
 --
 ALTER TABLE `GiftExchange`
+  ADD PRIMARY KEY (`GiftExchangeID`),
   ADD KEY `GiftGroupID` (`GiftGroupID`);
 
+--
+-- Indexes for table `GroupList`
+--
+ALTER TABLE `GroupList`
+  ADD PRIMARY KEY (`GroupID`),
+  ADD KEY `OwnerID` (`OwnerID`);
 
 --
 -- Indexes for table `GroupMember`
@@ -397,6 +392,22 @@ ALTER TABLE `GroupMember`
   ADD PRIMARY KEY (`GroupMemberID`),
   ADD KEY `GroupID` (`GroupID`),
   ADD KEY `MemberID` (`MemberID`);
+
+--
+-- Indexes for table `JoinRequests`
+--
+ALTER TABLE `JoinRequests`
+  ADD PRIMARY KEY (`RequestID`),
+  ADD KEY `MemberID` (`MemberID`),
+  ADD KEY `joinrequests_ibfk_1` (`GroupID`);
+
+--
+-- Indexes for table `Member`
+--
+ALTER TABLE `Member`
+  ADD PRIMARY KEY (`MemberID`),
+  ADD UNIQUE KEY `UniqueEmail` (`Email`),
+  ADD UNIQUE KEY `UniqueUsername` (`Username`) USING BTREE;
 
 --
 -- Indexes for table `Message`
@@ -410,6 +421,20 @@ ALTER TABLE `Message`
 -- Indexes for table `Post`
 --
 ALTER TABLE `Post`
+  ADD PRIMARY KEY (`PostID`),
+  ADD KEY `MemberID` (`MemberID`);
+
+--
+-- Indexes for table `Privacy`
+--
+ALTER TABLE `Privacy`
+  ADD PRIMARY KEY (`PrivacyID`);
+
+--
+-- Indexes for table `PromotionRequests`
+--
+ALTER TABLE `PromotionRequests`
+  ADD PRIMARY KEY (`RequestID`),
   ADD KEY `MemberID` (`MemberID`);
 
 --
@@ -433,7 +458,6 @@ ALTER TABLE `AccessControl`
 --
 -- AUTO_INCREMENT for table `Blocked`
 --
-
 ALTER TABLE `Blocked`
   MODIFY `BlockedID` int(50) NOT NULL AUTO_INCREMENT;
 
@@ -468,10 +492,10 @@ ALTER TABLE `Friendship`
   MODIFY `FriendshipID` int(50) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `Privacy`
+-- AUTO_INCREMENT for table `Gift`
 --
-ALTER TABLE `Privacy`
-  MODIFY `PrivacyID` int(50) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `Gift`
+  MODIFY `GiftID` int(50) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `GiftExchange`
@@ -491,13 +515,17 @@ ALTER TABLE `GroupList`
 ALTER TABLE `GroupMember`
   MODIFY `GroupMemberID` int(50) NOT NULL AUTO_INCREMENT;
 
-ALTER TABLE `JoinRequests` DROP FOREIGN KEY `joinrequests_ibfk_2`;
+--
+-- AUTO_INCREMENT for table `JoinRequests`
+--
+ALTER TABLE `JoinRequests`
+  MODIFY `RequestID` int(50) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `Member`
 --
 ALTER TABLE `Member`
   MODIFY `MemberID` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
 
 --
 -- AUTO_INCREMENT for table `Message`
@@ -510,6 +538,18 @@ ALTER TABLE `Message`
 --
 ALTER TABLE `Post`
   MODIFY `PostID` int(50) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Privacy`
+--
+ALTER TABLE `Privacy`
+  MODIFY `PrivacyID` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `PromotionRequests`
+--
+ALTER TABLE `PromotionRequests`
+  MODIFY `RequestID` int(50) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `Vote`
@@ -528,23 +568,12 @@ ALTER TABLE `AccessControl`
   ADD CONSTRAINT `accesscontrol_ibfk_1` FOREIGN KEY (`PostID`) REFERENCES `Post` (`PostID`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `accesscontrol_ibfk_2` FOREIGN KEY (`GroupID`) REFERENCES `GroupList` (`GroupID`) ON DELETE CASCADE ON UPDATE CASCADE;
 
--- Modify MemberID column
-ALTER TABLE `Member`
-  MODIFY `MemberID` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
-ALTER TABLE `Blocked`
-  ADD CONSTRAINT `blocked_ibfk_1` FOREIGN KEY (`MemberID1`) REFERENCES `Member`(`MemberID`) ON DELETE CASCADE ON UPDATE CASCADE;
-
-ALTER TABLE `Blocked`
-  ADD CONSTRAINT `blocked_ibfk_2` FOREIGN KEY (`MemberID2`) REFERENCES `Member`(`MemberID`) ON DELETE CASCADE ON UPDATE CASCADE;
-
-
 --
--- Constraints for table `Privacy`
+-- Constraints for table `Blocked`
 --
-ALTER TABLE `Privacy`
-  ADD CONSTRAINT `privacy_ibfk_1` FOREIGN KEY (`PrivacyID`) REFERENCES `Member` (`MemberID`) ON DELETE CASCADE ON UPDATE CASCADE;
-COMMIT;
+ALTER TABLE `Blocked`
+  ADD CONSTRAINT `blocked_ibfk_1` FOREIGN KEY (`MemberID1`) REFERENCES `Member` (`MemberID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `blocked_ibfk_2` FOREIGN KEY (`MemberID2`) REFERENCES `Member` (`MemberID`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `Comment`
@@ -607,6 +636,12 @@ ALTER TABLE `GroupMember`
   ADD CONSTRAINT `groupmember_ibfk_2` FOREIGN KEY (`MemberID`) REFERENCES `Member` (`MemberID`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
+-- Constraints for table `JoinRequests`
+--
+ALTER TABLE `JoinRequests`
+  ADD CONSTRAINT `joinrequests_ibfk_1` FOREIGN KEY (`GroupID`) REFERENCES `GroupList` (`GroupID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
 -- Constraints for table `Message`
 --
 ALTER TABLE `Message`
@@ -620,16 +655,24 @@ ALTER TABLE `Post`
   ADD CONSTRAINT `post_ibfk_1` FOREIGN KEY (`MemberID`) REFERENCES `Member` (`MemberID`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
+-- Constraints for table `Privacy`
+--
+ALTER TABLE `Privacy`
+  ADD CONSTRAINT `privacy_ibfk_1` FOREIGN KEY (`PrivacyID`) REFERENCES `Member` (`MemberID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `PromotionRequests`
+--
+ALTER TABLE `PromotionRequests`
+  ADD CONSTRAINT `promotionrequests_ibfk_1` FOREIGN KEY (`MemberID`) REFERENCES `Member` (`MemberID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
 -- Constraints for table `Vote`
 --
 ALTER TABLE `Vote`
   ADD CONSTRAINT `vote_ibfk_1` FOREIGN KEY (`VoterID`) REFERENCES `Member` (`MemberID`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `vote_ibfk_2` FOREIGN KEY (`SelectedOptionID`) REFERENCES `EventOptions` (`OptionID`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
-
-ALTER TABLE `Member`
-ADD COLUMN ProfilePic VARCHAR(255) DEFAULT 'uploads/images/default_pfp.png';
-
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
